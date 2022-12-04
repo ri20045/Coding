dd8587284f306e43b591ca6811ad58a8b815d936
ri20045 <116284592+ri20045@users.noreply.github.com>
2022-12-04 16:03:00 +0530

    Update count_consecutive_1s.cpp

--- C++/count_consecutive_1s.cpp
+++ C++/count_consecutive_1s.cpp
@@ -5,22 +5,28 @@ using namespace std;
 
 int main()
 {
-   string s = "1100011100011111";
+   string s;
+   cin >> s;
    int count = 0;
    int max = 0;
    for(int i=0; i<s.length(); i++){
       if(s[i] == '1'){
          count++;
       }
-      else if(count > max){
-         max = count;
-      }
+      
       else if(s[i] == '0'){
-         count = 0;
+         if(count > max){
+          max = count;
+
+      }
+       count = 0;
       }   
       
    }
-   max = count;
+   if(count > max){
+          max = count;
+
+      }
    cout << "max " << max;
    
    return 0;
