e4ddd1d6a331c2008e1eeb4db4634331a9b72d87
ri20045 <116284592+ri20045@users.noreply.github.com>
2022-12-09 18:51:38 +0530

    Create constructor.cpp

---
+++ C++/constructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class complex{
+   int a,b;
+public:
+   complex(void);
+      void printNumber()
+      {
+         cout << "Your Number is " << a << "+" << b << "i" << endl;  
+      }
+ 
+};
+
+complex :: complex(void){
+   a=10;
+   b=0;
+}
+int main(){
+
+complex c;
+c.printNumber();
+
+   return 0;
+}
+
