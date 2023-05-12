ef268e3270d4c930fc68351b717947119f7d30e6
ri20045 <116284592+ri20045@users.noreply.github.com>
2023-05-12 14:14:16 +0530

    Create Anonymous_Array.java

---
+++ JAVA/Anonymous_Array.java
@@ -0,0 +1,15 @@
+class Add
+{
+    public static void main(String[] args)
+    {
+       sum(new int[] {10,20,30,40});
+    }
+    public static void sum(int[] x)
+    {
+        int total = 0;
+        for(int x1 : x){
+            total = total + x1;
+        }
+        System.out.println("The sum: "+total);
+    }
+}
