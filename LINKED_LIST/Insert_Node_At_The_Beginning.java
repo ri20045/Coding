452b14829ced14a5fb792383d7b81a2e91c03006
ri20045 <116284592+ri20045@users.noreply.github.com>
2024-02-01 22:22:30 +0530

    Create Insert_Node_At_The_Beginning.java

---
+++ LINKED_LIST/Insert_Node_At_The_Beginning.java
@@ -0,0 +1,44 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node insertAtFirst(Node list, int newValue) {
+        // Write your code here
+        Node temp = new Node();
+        temp.data = newValue;
+        temp.next = list;
+        list = temp;
+        return list;
+    }
+}
